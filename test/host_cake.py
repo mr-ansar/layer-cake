d8ef2af2d41546e13e4d45a16c41a01b71ba7ef3
Scott Woods <scott.suzuki@gmail.com>
2025-04-30 18:01:43 +1200

    Initial version.

---
+++ test/host_cake.py
@@ -0,0 +1,66 @@
+# Author: Scott Woods <scott.18.ansar@gmail.com>
+# MIT License
+#
+# Copyright (c) 2017-2023 Scott Woods
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Directory at the HOST scope.
+
+.
+"""
+__docformat__ = 'restructuredtext'
+
+import layer_cake as lc
+
+#
+#
+class INITIAL: pass
+class RUNNING: pass
+
+class Host(lc.Point, lc.StateMachine):
+	def __init__(self):
+		lc.Point.__init__(self)
+		lc.StateMachine.__init__(self, INITIAL)
+
+def Host_INITIAL_Start(self, message):
+	return RUNNING
+
+def Host_RUNNING_Faulted(self, message):
+	self.complete(message)
+
+def Host_RUNNING_Stop(self, message):
+	self.complete(lc.Aborted())
+
+HOST_DISPATCH = {
+	INITIAL: (
+		(lc.Start,),
+		()
+	),
+	RUNNING: (
+		(lc.Faulted, lc.Stop),
+		()
+	),
+}
+
+lc.bind(Host, HOST_DISPATCH)
+
+
+if __name__ == '__main__':
+	lc.create(Host, scope=lc.ScopeOfDirectory.HOST)
