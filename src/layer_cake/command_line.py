a6ada79a6c676dec60402c3e112586d4795ac46f
Scott Woods <scott.suzuki@gmail.com>
2025-05-27 05:24:28 +1200

    Not using tmp_path.

--- src/layer_cake/command_line.py
+++ src/layer_cake/command_line.py
@@ -102,7 +102,7 @@ class CommandLine(object):
 			full_output: bool=False,
 			debug_level=None,
 			home_path: str=None, role_name: str=None,
-			model_path: str=None, tmp_path: str=None, resource_path: str=None,
+			model_path: str=None, resource_path: str=None,
 			help: bool=False,
 			create_role: bool=False, update_role: bool=False,
 			factory_reset: bool=False,
@@ -123,7 +123,6 @@ class CommandLine(object):
 		self.home_path = home_path
 		self.role_name = role_name
 		self.model_path = model_path
-		self.tmp_path = tmp_path
 		self.resource_path = resource_path
 		self.help = help
 		self.create_role = create_role
