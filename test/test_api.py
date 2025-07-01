fc073e018c05a43d1b36a8cd8f9ef9ea30b97345
Scott Woods <scott.suzuki@gmail.com>
2025-07-01 19:34:05 +1200

    Clean up stuff that didnt end up in demos.

--- test/test_api.py
+++ test/test_api.py
@@ -1,33 +1,11 @@
 # test_api.py
 import layer_cake as lc
-from enum import Enum
-
-__all__ = [
-	'table_type',
-	'CallingConvention',
-	'DEFAULT_CONVENTION',
-	'Xy',
-]
-
-table_type = lc.def_type(list[list[float]])
-
-class CallingConvention(Enum):
-	CALL=1
-	THREAD=2
-	THREAD_SPOOL=3
-	PROCESS=4
-	LIBRARY=5
-	PROCESS_SPOOL=6
-	FLOOD=7
-	SOAK=8
-
-DEFAULT_CONVENTION = CallingConvention.CALL
 
 class Xy(object):
-	def __init__(self, x: int=1, y: int=1, convention: CallingConvention=DEFAULT_CONVENTION):
+	def __init__(self, x: int=1, y: int=1):
 		self.x = x
 		self.y = y
-		self.convention = convention
 
 lc.bind(Xy)
 
+table_type = lc.def_type(list[list[float]])
