ba3ef03498639a828330701d0226f11edb6963a3
Scott Woods <scott.suzuki@gmail.com>
2025-07-01 19:28:11 +1200

    Stronger+simpler after testing - just use responsiveness as boundary, added busy_fraction.

--- src/layer_cake/object_spool.py
+++ src/layer_cake/object_spool.py
@@ -48,11 +48,26 @@ from .process_object import *
 from .get_response import *
 
 __all__ = [
+	'JoinSpool',
+	'LeaveSpool',
 	'ObjectSpool',
 ]
 
 #
 #
+class JoinSpool(object):
+	def __init__(self, worker_address: Address=None, role_name: str=None):
+		self.worker_address = worker_address
+		self.role_name = role_name
+
+class LeaveSpool(object):
+	def __init__(self, worker_address: Address=None, role_name: str=None):
+		self.worker_address = worker_address
+		self.role_name = role_name
+
+bind(JoinSpool)
+bind(LeaveSpool)
+
 class INITIAL: pass
 class PENDING: pass
 class EXECUTING: pass
@@ -67,57 +82,57 @@ class ObjectSpool(Point, StateMachine):
 	:param name: name of the executable file
 	:type name: str
 	"""
-	def __init__(self, object_type, *args, role_name=None, object_count=8, size_of_spool=512, responsiveness=None, **settings):
+	def __init__(self, object_type, *args, role_name=None, object_count=8, size_of_queue=None, responsiveness=None, busy_fraction=10, stand_down=30.0, **settings):
 		Point.__init__(self)
 		StateMachine.__init__(self, INITIAL)
 		self.object_type = object_type
 		self.args = args
 		self.role_name = role_name
 		self.object_count = object_count
-		self.size_of_spool = size_of_spool
+		self.size_of_queue = size_of_queue
 		self.responsiveness = responsiveness
+		self.busy_fraction = busy_fraction
+		self.stand_down = stand_down
 		self.settings = settings
 
 		self.idle_object = deque()
 		self.pending_request = deque()
+		self.forgetten_object = set()
 		self.span = deque()
 		self.total_span = 0.0
 		self.average = 0.0
-		self.boundary_1 = 0.0
-		self.boundary_2 = 0.0
 		self.shard = 0
 
-	def submit_request(self, message, forward_response, return_address):
+	def submit_request(self, message, forward_response, return_address, presented):
 		if self.responsiveness is None:
 			pass
-		elif self.average < self.boundary_1:
+		elif self.average < self.responsiveness:
 			pass
 		else:
 			self.shard += 1
-			divisor = 4 if self.average < self.boundary_2 else 10
-			if self.shard % divisor == 0:
-				self.send(Overloaded('Reduced responsiveness from spool objects'), return_address)
+			if self.shard % self.busy_fraction:
+				self.send(Busy(f'message rejected by spool (average response time {self.average:.2f})'), return_address)
 				return
 
 		idle = self.idle_object.popleft()
 		r = self.create(GetResponse, message, idle)
-		self.on_return(r, forward_response, idle=idle, return_address=return_address, started=clock_now())
+		self.on_return(r, forward_response, idle=idle, return_address=return_address, started=presented)
 
 def ObjectSpool_INITIAL_Start(self, message):
-	if self.responsiveness is not None:
-		self.boundary_1 = self.responsiveness % 0.75
-		self.boundary_2 = self.responsiveness % 0.9
-
-	pc = self.object_count
-	sos = self.size_of_spool
+	oc = self.object_count
+	sos = self.size_of_queue
 	r = self.responsiveness
+	sd = self.stand_down
 
-	if pc < 1 or (sos is not None and sos < 1) or (r is not None and r < 0.5):
-		self.complete(Faulted(f'cannot start the spool with the given parameters (count={pc}, size={sos}, responsiveness={r})'))
+	if oc < 1 or (sos is not None and sos < 1) or (r is not None and r < 0.5) or (sd is not None and sd < 2.0):
+		self.complete(Faulted(f'unexpected parameters (count={oc}, size={sos}, responsiveness={r}), stand_down={sd})'))
+
+	if self.object_type is None:
+		return SPOOLING
 
 	role_name = self.role_name
 
-	for i in range(pc):
+	for i in range(oc):
 		if role_name:
 			r = role_name.format(i=i)
 			a = self.create(self.object_type, *self.args, role_name=r, **self.settings)
@@ -129,43 +144,70 @@ def ObjectSpool_INITIAL_Start(self, message):
 
 	return SPOOLING
 
-def ObjectSpool_SPOOLING_Unknown(self, message):
-	m = cast_to(message, self.received_type)
-	if not self.idle_object:
-		if self.size_of_spool is None or len(self.pending_request) < self.size_of_spool:
-			self.pending_request.append((m, self.return_address))
-			return SPOOLING
-		request = portable_to_signature(self.received_type)
-		self.reply(Overloaded('Process resources busy and spool full', request=request))
-		return SPOOLING
-
-	def forward_response(self, value, kv):
-		# Completion of a request/responsesequence.
-		# Record the idle process.
+def forward_response(self, value, kv):
+	# Completion of a request/responsesequence.
+	# Record the idle process.
+	forgetten_object = True
+	try:
+		self.forgetten_object.remove(kv.idle)
+	except KeyError:
+		forgetten_object = False
 		self.idle_object.append(kv.idle)
 
-		# Update the performance metric.
-		span = clock_now() - kv.started
-		self.total_span += span
-		self.span.append(span)
-		while len(self.span) > SPOOL_SPAN:
-			s = self.span.popleft()
-			self.total_span -= s
-		self.average = self.total_span / len(self.span)
+	# Update the performance metric.
+	span = clock_now() - kv.started
+	self.total_span += span
+	self.span.append(span)
+	while len(self.span) > SPOOL_SPAN:
+		s = self.span.popleft()
+		self.total_span -= s
+	self.average = self.total_span / len(self.span)
 
-		# Deliver reponse to the original client.
-		m = cast_to(value, self.returned_type)
-		self.send(m, kv.return_address)
-		if not self.pending_request:
-			return
-		message, return_address = self.pending_request.popleft()
-
-		# There is a request-to-go and an available process.
-		self.submit_request(message, forward_response, return_address)
+	# Deliver reponse to the original client.
+	m = cast_to(value, self.returned_type)
+	self.send(m, kv.return_address)
+	if forgetten_object:
 		return
+	if not self.pending_request:
+		return
+	message, return_address, presented = self.pending_request.popleft()
 
 	# There is a request-to-go and an available process.
-	self.submit_request(m, forward_response, self.return_address)
+	self.submit_request(message, forward_response, return_address, presented)
+
+def ObjectSpool_SPOOLING_JoinSpool(self, message):
+	self.idle_object.append(message.worker_address)
+	if not self.pending_request:
+		return SPOOLING
+	message, return_address, presented = self.pending_request.popleft()
+
+	# There is a request-to-go and an available process.
+	self.submit_request(message, forward_response, return_address, presented)
+	return SPOOLING
+
+def ObjectSpool_SPOOLING_LeaveSpool(self, message):
+	a = message.worker_address
+	try:
+		self.idle_object.remove(a)
+	except ValueError:
+		self.forgetten_object.add(a)
+
+	# WHAT ABOUT ACTIVE GETRESPONSE!!!
+	return SPOOLING
+
+def ObjectSpool_SPOOLING_Unknown(self, message):
+	m = cast_to(message, self.received_type)
+	t = clock_now()
+	if not self.idle_object:
+		len_pending = len(self.pending_request)
+		if self.size_of_queue is None or len_pending < self.size_of_queue:
+			self.pending_request.append((m, self.return_address, t))
+			return SPOOLING
+		self.reply(Overloaded(f'message rejected by spool (overloaded, {len_pending} pending)'))
+		return SPOOLING
+
+	# There is a request-to-go and an available process.
+	self.submit_request(m, forward_response, self.return_address, t)
 	return SPOOLING
 
 def ObjectSpool_SPOOLING_Returned(self, message):
@@ -173,9 +215,17 @@ def ObjectSpool_SPOOLING_Returned(self, message):
 	if isinstance(d, OnReturned):
 		d(self, message)
 		return SPOOLING
+
+	if self.object_type is None:
+		return SPOOLING
+
 	self.trace(f'Spool process termination', returned_value=message.value)
 
-	seconds = spread_out(32.0)
+	stand_down = self.stand_down
+	if stand_down is None:
+		self.abort()
+		return CLEARING
+	seconds = spread_out(stand_down)
 
 	def restart(self, value, args):
 		role_name = args.role_name
@@ -207,7 +257,7 @@ OBJECT_SPOOL_DISPATCH = {
 		()
 	),
 	SPOOLING: (
-		(Unknown, Returned, Stop,),
+		(JoinSpool, LeaveSpool, Unknown, Returned, Stop),
 		()
 	),
 	CLEARING: (
